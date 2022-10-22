320a54072943bd6369caa2e20cea7cf27e5728d0
Nusaiba Muhammad Iya <nusaibaiya@gmail.com>
2022-10-22 13:15:00 +0100

    save

--- src/Manufacturer/Settings.js
+++ src/Manufacturer/Settings.js
@@ -1,12 +1,75 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from "react";
+import { Card, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function Settings() {
-    return (
-        <div>
-            <Card className='man_card shadow p-3'>
-                <h3 className='man_card_title'>Settings</h3>
-            </Card>
+  const navigate = useNavigate();
+  return (
+    <div>
+      {/* ********************************************** */}
+      <Card className="man_card shadow p-3">
+        <h3 className="man_card_title">Settings</h3>
+        <h4 className="mt-4">Update Profile</h4>
+
+        <Row className="mt-3">
+          <Col md={6}>
+            <label>Manufacturer License</label>
+            <input
+              name="manufacturer_license"
+              className="man_input_fields"
+              type="file"
+            />
+          </Col>
+          <Col md={6}>
+            <label>Pharmacist License</label>
+            <input
+              name="manufacturer_license"
+              className="man_input_fields"
+              type="file"
+            />
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col md={6}>
+            <label>Company Name</label>
+            <input
+              name="manufacturer_license"
+              className="man_input_fields"
+              type="text"
+            />
+          </Col>
+          <Col md={6}>
+            <label>Company Address</label>
+            <input
+              name="manufacturer_license"
+              className="man_input_fields"
+              type="address"
+            />
+          </Col>
+        </Row>
+
+        <Row className="mt-3">
+          <Col md={6}>
+            <label>Email</label>
+            <input
+              name="manufacturer_license"
+              className="man_input_fields"
+              type="email"
+            />
+          </Col>
+          <Col md={6}>
+            <label>Phone</label>
+            <input
+              name="manufacturer_license"
+              className="man_input_fields"
+              type="number"
+            />
+          </Col>
+        </Row>
+        <div className="mt-3">
+          <button className="man_button p-3">Save</button>
         </div>
-    )
+      </Card>
+    </div>
+  );
 }
