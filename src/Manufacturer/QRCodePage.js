1901235775431ba7aa80cf87f92f404c0b35a5f0
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2022-10-24 16:44:51 +0100

    save

--- src/Manufacturer/QRCodePage.js
+++ src/Manufacturer/QRCodePage.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import { Card, Col, Row } from 'react-bootstrap'
 import drugcipherIcon from '../image/DRUG CIPHER (2).png'
+import qrBackground from '../image/DRUG CIPHER (1).png'
 import { v4 as uuid4 } from "uuid";
 import Button from '../CustomFiles/Button'
 // import QRCode from 'react-qr-code';
@@ -52,12 +53,45 @@ export default function QRCodePage() {
                 <h3 className='man_card_title'>Drug QR Code</h3>
                 <Row>
                     <Col md={4}>
-                        <div className='qr_code_div' style={{ marginTop: 40 }}>
+                        <div className='qr_code_div1' style={{ marginTop: 40 }}>
                             {/* <input type='text' name='drugName' value={drug.drugName} onChange={handleChange} /> */}
                             {/* <QRCode id="qrCodeEl" value={drugDetails} size={185} />, */}
-                            <QRCode id="qr-gen" value={drugDetails} level={"H"}
-                                includeMargin={true} logoImage={drugcipherIcon} logoWidth={50} qrStyle={'dots'} size={300} removeQrCodeBehindLogo={true} enableCORS={true}/>
+                            <QRCode
+                                id="qr-gen"
+                                value={drugDetails}
+                                level={"H"}
+                                includeMargin={true}
+                                logoImage={drugcipherIcon}
+                                logoWidth={100} qrStyle={'dots'}
+                                size={300}
+                                // removeQrCodeBehindLogo={true}
+                                enableCORS={true}
+                                logoOpacity={1}
+                                eyeRadius={[
+                                    [30, 0, 0, 0],
+                                    [0, 30, 0, 0],
+                                    [0, 0, 0, 30],
+                                ]}
+                                eyeColor={[
+                                    {
+                                        outer: 'black',
+                                        inner: 'rgb(3, 66, 110)',
+                                    },
+                                    {
+                                        outer: 'black',
+                                        inner: 'rgb(3, 66, 110)',
+                                    },
+                                    {
+                                        outer: 'black',
+                                        inner: 'rgb(3, 66, 110)',
+                                    }
+
+                                ]}
+                            />
                         </div>
+                        {/* <div className='qr_code_div2'>
+                            <img src={qrBackground} style={{ width: 300 }} />
+                        </div> */}
                     </Col>
                 </Row>
                 <div className='mt-3'>
