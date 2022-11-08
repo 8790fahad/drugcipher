36bd71804262fd9f57dbfa7c6dfcebdab7d7cb1b
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2022-11-08 22:59:28 +0100

    save

--- src/Manufacturer/KYC/Final.js
+++ src/Manufacturer/KYC/Final.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { Card, Col, Row } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import logo from "../../image/DRUG CIPHER (2).png";
-import emoji from "../../image/emoji.png";
+// import emoji from "../../image/emoji.png";
+import emoji from "../../image/Bcgr6Xj7i.gif";
+
 
 export default function Final() {
+    
+    const currentYear = year.getFullYear()
     const navigate = useNavigate()
     return (
         <div className='container'>
@@ -37,13 +41,24 @@ export default function Final() {
                     </Row>
                     <div className='text-center p-5'>
                         <h1 className='thankyou'>Thank you for registering</h1>
-                        <div className='emoji_div shadow'>
-                            <img src={emoji} alt='' className='emoji' />
-                        </div>
+                        <img src={emoji} alt='' className='emoji' />
+                        
                         <p className='mssg'>Your account is under review. when it is approved, a link will be sent to your email that will allow you to generate a passphrase and login to your account.</p>
                     </div>
                 </div>
             </Card>
+            <Row className='m-0 lastfootersection'>
+                <Col md={4}><p>Copyright © {currentYear} DrugCipher.
+                    All rights reserved.</p></Col>
+                <Col md={1}></Col>
+                <Col md={2} className='text-center'>
+                </Col>
+                <Col md={1}></Col>
+                <Col md={1}></Col>
+                <Col md={3}>
+                    <p>Privacy & Policy . Terms & Conditions</p>
+                </Col>
+            </Row>
         </div >
     )
 }
