f716f171335aed3b2012312f21d78225689dba4a
8790fahad <8790fahadado@gmail.com>
2022-11-23 17:22:50 +0100

    changes

--- src/Manufacturer/DrugsLocation.js
+++ src/Manufacturer/DrugsLocation.js
@@ -20,12 +20,13 @@ export default function DrugsLocation({ location = [] }) {
         center={[12.011171324972095, 8.542933227726762]}
         zoom={[1]}
       >
-        {location.length &&
-         location.map((state) => (
-            <Marker coordinates={[state?.latitude, state?.longitude]}>
-              <MapPin />
-            </Marker>
-          ))}
+        {location.length && location[0] !== null
+          ? location.map((state) => (
+              <Marker coordinates={[state?.latitude, state?.longitude]}>
+                <MapPin />
+              </Marker>
+            ))
+          : null}
 
         <ZoomControl />
       </Map>
