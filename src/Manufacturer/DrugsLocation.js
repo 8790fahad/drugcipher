d96d41ce3be29e5d1b664b162b5e82ae2d82dcd7
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2022-11-24 17:59:06 +0100

    save

--- src/Manufacturer/DrugsLocation.js
+++ src/Manufacturer/DrugsLocation.js
@@ -20,7 +20,7 @@ export default function DrugsLocation({ location = [] }) {
         center={[12.011171324972095, 8.542933227726762]}
         zoom={[1]}
       >
-        {location.length && location[0] === null
+        {location.length && location[0] == null
           ?
           null : location.map((state) => (
             <Marker coordinates={[state?.latitude, state?.longitude]}>
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -25,9 +25,10 @@ export function logout() {
 export const WALLET_ID = process.env.WALLET_ID || "drugcipher.near";
 
 const serverUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://192.168.43.244:34561"
-    : "https://yge.wvi.mybluehost.me/test/drug-cipher-server";
+  // process.env.NODE_ENV === "development"
+  //   ? "http://192.168.43.244:34561"
+  //   :
+     "https://yge.wvi.mybluehost.me/test/drug-cipher-server";
 export const apiURL = serverUrl + "/api";
 
 const _fetchApi = (
